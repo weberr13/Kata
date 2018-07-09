6d4554d794b55168f060d1d3eb233e12ff85901d
Robert Weber <weberr@admins-MacBook-Air.local>
2018-07-09 14:19:48 -0600

    a test to show rebalances don't lose data

--- algorithms/test/TestHashTable.cpp
+++ algorithms/test/TestHashTable.cpp
@@ -8,11 +8,19 @@ TEST_F(TestHashTable, put_some_stuff) {
     auto r = aTable.Get(1);
     EXPECT_TRUE(r.ok);
     EXPECT_EQ(r.value, 128);
+    aTable.Put(2, 33);
+    r = aTable.Get(1);
+    EXPECT_TRUE(r.ok);
+    EXPECT_EQ(r.value, 128);  
+    r = aTable.Get(2);
+    EXPECT_TRUE(r.ok);
+    EXPECT_EQ(r.value, 33);   
 }
 
 TEST_F(TestHashTable, put_lots_of_stuff) {
     HashTable<int, int> aTable;
     int iterations = 10000;
+    aTable.Put(-1, 128);
 
     for (int i = 0 ; i < iterations ; i++) {
         int index = std::rand()%100000;
@@ -21,5 +29,7 @@ TEST_F(TestHashTable, put_lots_of_stuff) {
         EXPECT_TRUE(r.ok);
         EXPECT_EQ(r.value, i);
     }
-
+    auto r = aTable.Get(-1);
+    EXPECT_TRUE(r.ok);
+    EXPECT_EQ(r.value, 128);
 }
